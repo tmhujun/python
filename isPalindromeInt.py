783cb81fda376b11ca5e266ec8613079a4cf9fdd
tmhujun <david.hu.j@gmail.com>
2018-05-23 21:36:55 -0700

    Delete isPalindromeInt.py

--- isPalindromeInt.py
+++
@@ -1,1 +0,0 @@
-#! python3# isPalindromeInt.py - check if an integer is palindromedef isPalindrome(self, x):    """    :type x: int    :rtype: bool    """    if x < 0:        return False    else:        passdef main():    isPalindrome(100)if __name__ == "__main__":    main()
